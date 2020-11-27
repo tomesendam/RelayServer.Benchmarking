f2120ee0a9f316ee12a37e7c25c39d235a191b7c
TeBeCo <tebeco@gmail.com>
2020-11-27 01:29:41 +0100

    Create POCO for Messages

---
+++ src/TerminalGame.RelayServer.WithBedrock/Messages.cs
@@ -0,0 +1,14 @@
+﻿namespace TerminalGame.RelayServer.WithBedrock
+{
+    public enum MyPayloadType
+    {
+        Init,
+        Payload
+    }
+
+    public abstract record MyRequestMessage(string Source, MyPayloadType PayloadType);
+
+    public record InitMessage(string Source) : MyRequestMessage(Source, MyPayloadType.Init);
+    public record PayloadMessage(string Source, string Destination, string Payload) : MyRequestMessage(Source, MyPayloadType.Payload);
+
+}
