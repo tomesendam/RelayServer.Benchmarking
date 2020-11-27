bbea3570d1591ef3be0bf222831954917cd0523f
TeBeCo <tebeco@gmail.com>
2020-11-27 01:36:11 +0100

    re-write Client.SendAsync() properly

--- src/TerminalGame.RelayServer.WithBedrock/ClientWorker.cs
+++ src/TerminalGame.RelayServer.WithBedrock/ClientWorker.cs
@@ -15,7 +15,6 @@ namespace TerminalGame.RelayServer.WithBedrock
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly IHostApplicationLifetime _hostApplicationLifetime;
-        private Client? _client;
         private ConnectionContext? _connection;
 
         public ClientWorker(IServiceProvider serviceProvider, IHostApplicationLifetime hostApplicationLifetime)
@@ -28,57 +27,30 @@ namespace TerminalGame.RelayServer.WithBedrock
         {
             await Task.Yield();
 
-            _client = new ClientBuilder(_serviceProvider)
+            var client = new ClientBuilder(_serviceProvider)
                         .UseSockets()
                         .UseConnectionLogging("Client")
                         .Build();
 
-            _connection = await _client.ConnectAsync(new IPEndPoint(IPAddress.IPv6Loopback, 530), _hostApplicationLifetime.ApplicationStopping);
+            _connection = await client.ConnectAsync(new IPEndPoint(IPAddress.IPv6Loopback, 530), _hostApplicationLifetime.ApplicationStopping);
 
             if (_connection == null)
             {
                 return;
             }
 
-            var lines = new string[]
-            {
-                "{\"payloadType\":\"INIT\",\"source\":\"1\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload0\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload1\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload2\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload3\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload4\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload5\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload6\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload7\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload8\"}",
-                "{\"payloadType\":\"MESSAGE\",\"destination\":\"0\",\"source\":\"1\",\"payload\":\"Payload9\"}",
-            };
-
-            foreach (var line in lines)
-            {
-                WriteLineHeader(_connection, line);
-                WriteLine(_connection, Encoding.UTF8, line);
-            }
-
-            await _connection.Transport.Output.FlushAsync(_hostApplicationLifetime.ApplicationStopping);
-
-            static void WriteLineHeader(ConnectionContext connection, string line)
-            {
-                var sizeSpan = connection.Transport.Output.GetSpan(4);
-                BinaryPrimitives.WriteInt32BigEndian(sizeSpan, line.Length);
-
-                connection.Transport.Output.Write(sizeSpan[..4]);
-            }
-
-
-            static void WriteLine(ConnectionContext connection, Encoding encoding, string line)
-            {
-                var length = encoding.GetByteCount(line);
-                var payloadSpan = connection.Transport.Output.GetSpan(length);
-                encoding.GetBytes(line, payloadSpan);
-                connection.Transport.Output.Write(payloadSpan[..length]);
-            }
+            var protocol = new MyClientProtocol(_connection);
+            await protocol.SendAsync(new InitMessage("1"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload0"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload1"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload2"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload3"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload4"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload5"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload6"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload7"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload8"));
+            await protocol.SendAsync(new PayloadMessage("1", "0", "Payload9"));
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
