8c51a8f1ceb2daa19dd7dfebc14c8c998d457e8a
TeBeCo <tebeco@gmail.com>
2020-11-27 18:51:48 +0100

    dummy way to compute Message length

---
+++ src/TerminalGame.RelayServer.WithBedrock/MessageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace TerminalGame.RelayServer.WithBedrock
+{
+    public static class MyRequestMessageExtensions
+    {
+        public static int GetMessageLength(this MyRequestMessage message) =>
+            message switch
+            {
+                InitMessage msg => msg.GetMessageLength(),
+                PayloadMessage msg => msg.GetMessageLength(),
+                _ => throw new InvalidOperationException($"Unsupported message type: {message.GetType().FullName}")
+            };
+
+        public const string EmptyInitJsonMessage = "{\"payloadType\":\"INIT\",\"source\":\"\"}";
+        public static int GetMessageLength(this InitMessage message)
+        {
+            var sourceLength = Encoding.UTF8.GetByteCount(message.Source);
+
+            return EmptyInitJsonMessage.Length + sourceLength;
+        }
+
+        public const string EmptyPayloadJsonMessage = "{\"payloadType\":\"MESSAGE\",\"destination\":\"\",\"source\":\"\",\"payload\":\"\"}";
+        public static int GetMessageLength(this PayloadMessage message)
+        {
+            var sourceLength = Encoding.UTF8.GetByteCount(message.Source);
+            var destinationLength = Encoding.UTF8.GetByteCount(message.Destination);
+            var payloadLength = Encoding.UTF8.GetByteCount(message.Payload);
+
+            return EmptyPayloadJsonMessage.Length + sourceLength + destinationLength + payloadLength;
+        }
+    }
+}
--- src/TerminalGame.RelayServer.WithBedrock/Messages.cs
+++ src/TerminalGame.RelayServer.WithBedrock/Messages.cs
@@ -1,5 +1,22 @@
-﻿namespace TerminalGame.RelayServer.WithBedrock
+﻿using System.IO;
+
+namespace TerminalGame.RelayServer.WithBedrock
 {
+
+    public static class MyPayloadTypeStrings
+    {
+        public const string Init = "INIT";
+        public const string Payload = "MESSAGE";
+
+        public static string ToString(this MyPayloadType myPayloadType) =>
+            myPayloadType switch
+            {
+                MyPayloadType.Init => Init,
+                MyPayloadType.Payload => Payload,
+                _ => throw new InvalidDataException($"Expected '{nameof(MyPayloadType)}' to be of type {myPayloadType.GetType().Name}.")
+            };
+    }
+
     public enum MyPayloadType
     {
         Init,
@@ -10,5 +27,4 @@
 
     public record InitMessage(string Source) : MyRequestMessage(Source, MyPayloadType.Init);
     public record PayloadMessage(string Source, string Destination, string Payload) : MyRequestMessage(Source, MyPayloadType.Payload);
-
 }
